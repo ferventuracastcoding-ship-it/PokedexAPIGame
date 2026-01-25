ce26002ae1ffd41d750bf8559a72a6620d6c2657
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-01-24 20:04:44 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+entity.status();
+const entity = {
+  health: 0;
+  revive() {
+    this.health = 100;
+  },
+  status() {
+    this.revive();
+    console.log(this.health);
+  }
+};
+
+console.log(this);
